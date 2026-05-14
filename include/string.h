e068147fafacb1df5f6b490fe48a1438982c6e2a
sachintu47 <sachintu47@gmail.com>
2026-05-14 08:13:25 -0400

    Add stpncpy implementation

--- include/string.h
+++ include/string.h
@@ -71,6 +71,7 @@ extern "C" {
 __Z_EXPORT size_t strnlen(const char *, size_t );
 __Z_EXPORT char *strpcpy(char *, const char *);
 __Z_EXPORT char *stpcpy(char *dest, const char *src);
+__Z_EXPORT char *stpncpy(char *dest, const char *src, size_t n);
 __Z_EXPORT char *strsignal(int );
 
 __Z_EXPORT char *strndup(const char *s, size_t n);
